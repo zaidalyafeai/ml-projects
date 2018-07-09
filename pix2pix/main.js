058860085551e2a44cab1e7d8302c8b7ce73f97f
Zaid Alyafeai <alyafey22@gmail.com>
2018-07-09 05:06:58 +0300

    Update main.js

--- pix2pix/main.js
+++ pix2pix/main.js
@@ -114,7 +114,7 @@ function preprocess(imgData) {
     return tf.tidy(() => {
         //convert to a tensor 
         let tensor = tf.fromPixels(imgData).toFloat()
-        //tensor = tf.scalar(255).sub(tensor)
+        tensor = tf.scalar(255).sub(tensor)
         //resize 
         let resized = tf.image.resizeBilinear(tensor, [256, 256])
                 
