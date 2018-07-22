9dc063e297a785ccfdd618d3a87ed1918ff47d20
Zaid Alyafeai <alyafey22@gmail.com>
2018-07-22 05:28:45 +0300

    Update main.js

--- pix2pix/main.js
+++ pix2pix/main.js
@@ -104,7 +104,7 @@ function getFrame() {
         const gCanvas = document.getElementById('gCanvas');
         const postImg = postprocess(gImg)
         console.log(gCanvas.width)
-        tf.toPixels(postImg, gCanvas)
+        toImage(postImg, gCanvas)
     }
 
 }
@@ -181,6 +181,33 @@ function allowDrawing() {
     };
 }
 
+function toImage(tensor, canvas) {
+    const ctx = canvas.getContext('2d');
+    //get the tensor shape
+    const [height, width] = tensor.shape;
+    //create a buffer array
+    const buffer = new Uint8ClampedArray(width * height * 4)
+    //create an Image data var 
+    const imageData = new ImageData(width, height);
+    //get the tensor values as data
+    const data = tensor.dataSync();
+    //map the values to the buffer
+    var i = 0;
+    for(var y = 0; y < height; y++) {
+    for(var x = 0; x < width; x++) {
+        var pos = (y * width + x) * 4;      // position in buffer based on x and y
+        buffer[pos  ] = data[i]             // some R value [0, 255]
+        buffer[pos+1] = data[i+1]           // some G value
+        buffer[pos+2] = data[i+2]           // some B value
+        buffer[pos+3] = 255;                // set alpha channel
+        i+=3
+    }
+  }
+    //set the buffer to the image data
+    imageData.data.set(buffer)
+    //show the image on canvas
+    ctx.putImageData(imageData, 0, 0);
+  };
 /*
 clear the canvs 
 */
