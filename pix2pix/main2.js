b86984b52ae8bb162a5c952a8fe32acf8defc9d7
Zaid Alyafeai <alyafey22@gmail.com>
2018-07-23 01:02:04 +0300

    Update main2.js

--- pix2pix/main2.js
+++ pix2pix/main2.js
@@ -144,7 +144,7 @@ load the model
 */
 async function start() {
     //load the model 
-    model = await tf.loadModel('model9/model.json')
+    model = await tf.loadModel('model/model.json')
     
     //status 
     document.getElementById('status').innerHTML = 'Model Loaded';
