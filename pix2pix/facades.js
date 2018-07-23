8ef2ac5e0d5b22ef6709e9c2522bfea846f55845
Zaid Alyafeai <alyafey22@gmail.com>
2018-07-23 21:44:32 +0300

    Update facades.js

--- pix2pix/facades.js
+++ pix2pix/facades.js
@@ -83,8 +83,8 @@ function mouseup(e) {
 
     var square = canvas.getActiveObject();
     canvas.add(square); 
-    square.set({selectable:false, hasControls:false, hasBorders:false})
-    square.lockMovementX = square.lockMovementY = true
+    square.set({hasControls:false, hasBorders:false})
+    square.evented = false
     canvas.renderAll();
     getFrame();
  } 
