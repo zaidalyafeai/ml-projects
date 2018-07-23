fb02e558012aaa5d51cd9ef8ecf19dbf9599ddd0
Zaid Alyafeai <alyafey22@gmail.com>
2018-07-23 16:51:22 +0300

    Update facades.js

--- pix2pix/facades.js
+++ pix2pix/facades.js
@@ -162,7 +162,7 @@ load the model
 */
 async function start() {
     //load the model 
-    model = await tf.loadModel('shoes/model.json')
+    model = await tf.loadModel('facades/model.json')
     
     //status 
     document.getElementById('status').innerHTML = 'Model Loaded';
