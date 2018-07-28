48e32ed9363c82240907010fd217f6af6096a112
zaidalyafeai <alyafey22@gmail.com>
2018-07-28 15:10:57 +0300

    debug

--- pix2pix/main2.js
+++ pix2pix/main2.js
@@ -63,7 +63,7 @@ load the model
 */
 async function start() {
     //load the model 
-    model = await tf.loadModel('cats/model.json')
+    model = await tf.loadModel('model/model.json')
     
     model.layers[1].getWeights()[0].print()
     //status 
