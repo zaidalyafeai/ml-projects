3dd3b027091cbf96099a4e26ea7042eb3aaf6c1b
Zaid Alyafeai <alyafey22@gmail.com>
2018-07-31 13:58:26 +0300

    Update main.js

--- playground/main.js
+++ playground/main.js
@@ -1,7 +1,7 @@
 async function loadModel()
 {
   model = tf.loadModel('model/model.json')
-  model.predict(tf.zeros(1, 2, 2, 3)).print()
+  model.predict(tf.zeros([1, 2, 2, 3])).print()
 }
 
 loadModel()
